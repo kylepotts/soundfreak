548750d1699fbb0c132551b9aad983999810fc38
Kyle <pottsk@purdue.edu>
2015-07-25 19:30:49 -0400

    there ya go Tylor

--- app/src/main/java/com/deadbeef/soundfreq/tasks/FileDownloadTask.java
+++ app/src/main/java/com/deadbeef/soundfreq/tasks/FileDownloadTask.java
@@ -1,10 +1,17 @@
 package com.deadbeef.soundfreq.tasks;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Environment;
 import android.util.Log;
 
+import com.deadbeef.soundfreq.interfaces.OnFileDownloadedListener;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,60 +24,51 @@ import java.net.URL;
  */
 public class FileDownloadTask extends AsyncTask<Void, Void, Void>{
     private String stringUrl;
+    private String name;
+    private Context context;
+    private OnFileDownloadedListener listener;
 
-    public FileDownloadTask(String url){
+    public FileDownloadTask(String url, String name, Context context, OnFileDownloadedListener listener){
+        this.name = name;
         this.stringUrl = url;
+        this.context = context;
+        this.listener = listener;
     }
 
     @Override
     protected Void doInBackground(Void... params){
-        InputStream input = null;
-        OutputStream output = null;
-        HttpURLConnection connection = null;
-
         try {
-            URL url = new URL(stringUrl);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.connect();
-
-            if ( connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                return null;
-            }
-
-            input = connection.getInputStream();
-            output = new FileOutputStream(Environment.getExternalStorageDirectory());
-
-            byte data[] = new byte[10240];
-            int count;
-            while ((count = input.read(data)) != -1 ){
-                output.write(data, 0, count);
-            }
-
-        } catch (IOException e){
+            URL u = new URL(stringUrl);
+            DataInputStream stream = new DataInputStream(u.openStream());
+            byte[] buffer = IOUtils.toByteArray(stream);
+            FileOutputStream fos = context.openFileOutput(name, Context.MODE_PRIVATE);
+            fos.write(buffer);
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
             return null;
-        } finally {
-            try {
-                if ( output != null ){
-                    output.close();
-                }
-                if ( input != null ){
-                    input.close();
-                }
-            } catch (IOException e) {
-                return null;
-            }
-
-            if(connection != null){
-                connection.disconnect();
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         }
         return null;
     }
 
     @Override
     protected void onPostExecute(Void aVoid) {
-        File f = Environment.getExternalStorageDirectory();
-        Log.d("tylor", "File name: " + f);
+        FileOutputStream fos = null;
         super.onPostExecute(aVoid);
+        try {
+             fos = context.openFileOutput(name,Context.MODE_PRIVATE);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            listener.onFileDownloaded(fos.getFD());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
